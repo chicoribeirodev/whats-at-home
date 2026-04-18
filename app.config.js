a66207fbea0fd22d2e9e462e74bfb52e369ee199
Francisco Ribeiro <francisco@shuffle.energy>
2026-04-18 11:27:38 -0700

    fix app.config.js

--- app.config.js
+++ app.config.js
@@ -7,6 +7,9 @@ export default {
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: "41eed555-2d67-4739-a674-c3bb9912ff7f"
+      }
     },
   },
 };
